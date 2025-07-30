251b344d0c91de4b80a1a5e71bb207f5d7d7707b
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-30 17:14:20 +0300

    initial commit - Amarket project

---
+++ item/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from .models import Item
+
+INPUT_CLASS = 'w-full py-4 px-6 rounded border'
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASS
+            }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': INPUT_CLASS
+            })
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASS
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASS
+            }),
+            'image': forms.ClearableFileInput(attrs={
+                'class': INPUT_CLASS
+            })
+        }
